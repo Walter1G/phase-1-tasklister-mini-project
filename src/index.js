3d3b60caf6d0f7c4a4603e73fb12bc237265a851
Walter N <2760walter@gmail.com>
2023-09-27 20:14:25 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let todoForm = document.querySelector("form");
+  todoForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    makeTodo(document.getElementById("new-task-description").value);
+    document.getElementById("new-task-description").value="";
+    
+  })
+  
+
 });
+
+
+function makeTodo(todo){
+  let p = document.createElement("p");
+   p.textContent= `${todo} `;
+   let btn = document.createElement("button");
+   btn.textContent =" X";
+   p.appendChild(btn);
+   document.querySelector("#tasks").appendChild(p);
+
+   btn.addEventListener("click", deleteTodo);
+}
+
+function deleteTodo(e){
+  e.target.parentNode.remove();
+}
